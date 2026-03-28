e3609cfec7e90a2cb7a29234f76cccf24e3445c4
Rhythm <rhythsanjeev2006@gmail.com>
2026-03-28 19:22:57 +0530

    feat: State Management

---
+++ week10-rolling-up-state/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './App.css'
+// As your application grows, you find out that multiple components need to access the same state,
+// instead of duplicationg each state for each component, you can lift the state to the lowest common ancestor 
+// allowing the common ancestor to manage it.
+function App() {
+  
+
+  return (
+    <>
+      <LightBulb/>
+    </>
+  )
+}
+
+function LightBulb(){
+  const [isLightOn,setIsLightOn] = useState(true);
+  return <>
+  <BulbState isLightOn={isLightOn}/>
+  <BulbToggle setIsLightOn={setIsLightOn}/>
+  </>
+}
+
+function BulbState({isLightOn}){
+  return <>
+  {isLightOn?"Bulb On":"Bulb Off"}
+  </>
+}
+
+function BulbToggle({setIsLightOn}){
+  function toggleLight(){
+    setIsLightOn(currentState => !currentState)
+  }
+  return <>
+  <button onClick={toggleLight}>Toggle Light</button>
+  </>
+}
+
+export default App
